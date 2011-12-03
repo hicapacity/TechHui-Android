c4d554e0c76d99ecdaba1dddb0e1f5ca2f6fb934
Jason Axelson <jason.axelson@gmail.com>
2011-12-02 20:22:31 -1000

    Initial commit

---
+++ src/org/hicapacity/techhui/TechHuiActivity.java
@@ -0,0 +1,13 @@
+package org.hicapacity.techhui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TechHuiActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
